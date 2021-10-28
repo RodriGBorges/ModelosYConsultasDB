86970e1423dcea87b947591665a1a3f58cc44352
RodriGBorges <gabii.borges1998@gmail.com>
2021-10-28 20:03:09 -0300

    Funcion detail para cada pelicula

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -7,8 +7,16 @@ const controller = {
             res.render('moviesList', {movies: peliculas})
         })
         .catch(err => {
-            console.log('Error al requerir las películas de la base de datos. Erorr:', err)
+            console.log('Error al requerir las películas de la base de datos. Erorr:', err);
         })
+    },
+    detail: async (req, res) => {
+        try {
+            const pelicula = await db.movies.findByPk(parseInt(req.params.id))
+            res.render('moviesDetail', { movie: pelicula })
+        } catch (error) {
+            console.log('Error al requerir la película de la base de datos. Erorr:', error);
+        }
     }
 }
 
--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { list } = require('../controllers/moviesController');
+const { list, detail } = require('../controllers/moviesController');
 
 router.get('/movies', list);
 //router.get('/movies/new', moviesController.new);
 //router.get('/movies/recommended', moviesController.recomended);
-//router.get('/movies/detail/:id', moviesController.detail);
+router.get('/movies/detail/:id', detail);
 
 
 module.exports = router;
